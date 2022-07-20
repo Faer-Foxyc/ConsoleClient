63ccd7af843b1b1f01dfb5a66c0321b3c9629d5a
Антон Горлищев <anton_popov10@yahoo.com>
2022-07-20 16:48:39 +0300

    Обновление

---
+++ Class_Info_System.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Management;
+
+namespace ConsoleClient
+{
+    internal class Class_Info_System
+    {
+        public static string Bios_Info(string Version_Bios)
+        {
+            ManagementObjectSearcher managementObjectSearcher = new ManagementObjectSearcher("root\\CIMV2",
+                    "SELECT * FROM Win32_BIOS");
+            foreach (ManagementObject queryObj in managementObjectSearcher.Get())
+            {
+                 Version_Bios = "Version: " + queryObj["Version"];
+            }
+            return Version_Bios;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,13 +2,14 @@
 using System.Text;
 using System.Net.Sockets;
 using System.Threading;
+using System.Management;
 
 namespace ConsoleClient
 {
-    internal class Program
+    internal class Program : Class_Info_System
     {
         const int port = 8888;
-        const string address = "Adress local server";
+        const string address = "192.168.0.140";
 
         static void Main(string[] args)
         {
@@ -18,6 +19,8 @@ namespace ConsoleClient
             string UserDomainName = Environment.UserDomainName.ToString();
             string MachineName = Environment.MachineName.ToString();
             string ProcessorCount = Environment.ProcessorCount.ToString();
+            string Pc_Ram = "";
+            string Version_bios = "";
             
             TcpClient tcpClient = null;
             try
@@ -29,11 +32,12 @@ namespace ConsoleClient
                 {
 
 
-                    Console.WriteLine(userName + ": ");
+                    Console.WriteLine(userName + ": Передаю данные на сервер...");
                     // Ввод сообщения
                     //string message = Console.ReadLine();
-                    string message = String.Format("{0}: Версия Ос: {1}, Доменное имя: {2}, Имя компьютера: {3}, Количество процессоров: {4}"
-                        , userName, OsVersion, UserDomainName, MachineName, ProcessorCount);
+                    string message = String.Format("{0}: Версия Ос: {1}, Доменное имя: {2}, Имя компьютера: {3}, Количество процессоров: {4}, Оперативная память: {5}," +
+                        " Версия биос: {6}"
+                        , userName, OsVersion, UserDomainName, MachineName, ProcessorCount, Ram(Pc_Ram), Bios_Info(Version_bios));
                     // Преобразуем сообщение в массив байтов
                     byte[] data = Encoding.Unicode.GetBytes(message);
                     // Отправка сообщения
@@ -50,6 +54,18 @@ namespace ConsoleClient
             {
                 tcpClient.Close();
             }
+        }
+
+        static string Ram(string Memory)
+        {
+            ManagementObjectSearcher Ram_Monitor = new ManagementObjectSearcher("SELECT TotalVisibleMemorySize,FreePhysicalMemory FROM Win32_OperatingSystem");
+            foreach  (ManagementObject Obj_Ram in Ram_Monitor.Get())
+            {
+                ulong totalRam = Convert.ToUInt64(Obj_Ram["TotalVisibleMemorySize"]);
+                ulong busyRam = totalRam - Convert.ToUInt64(Obj_Ram["FreePhysicalMemory"]);
+                Memory = Convert.ToString((busyRam * 100) / totalRam);
+            }
+            return Memory;
         }
 
     }
